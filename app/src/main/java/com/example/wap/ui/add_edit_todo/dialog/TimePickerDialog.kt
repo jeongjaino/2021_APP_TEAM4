8629b3c1f764be661e229b6c375b4484bea63db9
jaino <msb3302@naver.com>
2022-01-21 16:56:56 +0900

    modify calendarView date format

--- app/src/main/java/com/example/wap/ui/add_edit_todo/dialog/TimePickerDialog.kt
+++ app/src/main/java/com/example/wap/ui/add_edit_todo/dialog/TimePickerDialog.kt
@@ -33,7 +33,7 @@ class TimePickerDialog : DialogFragment() {
         setTodoViewModel = ViewModelProvider(requireActivity())[SetTodoViewModel::class.java]
 
         binding.timePicker.setOnTimeChangedListener { picker, hour, min ->
-            setTodoViewModel.setTime(setTime(picker.is24HourView, hour, min))
+            setTodoViewModel.setTime(setTime(hour, min))
         }
 
         binding.timePickerYesButton.setOnClickListener{
@@ -50,14 +50,17 @@ class TimePickerDialog : DialogFragment() {
         return binding.root
     }
 
-    private fun setTime(is24Hour: Boolean, hour: Int, min: Int): String{
-        val is24 = if(is24Hour){
+    private fun setTime(hour: Int, min: Int): String{
+        val is24 = if(hour > 12){
             "오후"
         } else{
             "오전"
         }
         val inHour = if(hour >= 10){
-            "$hour"
+            if(hour > 12){
+                "${hour-12}"
+            }
+            else { "$hour" }
         } else{
             "0$hour"
         }
